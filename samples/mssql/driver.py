9c19a07c29f0e76002d22d1e8234556c83941085
ManhTCT <trancongtuanmanh@gmail.com>
2024-03-30 16:17:34 +0700

    Update database connection configurations

--- samples/mssql/driver.py
+++ samples/mssql/driver.py
@@ -1,13 +1,18 @@
-from inno_syncer.main import start_job
-from inno_syncer.sync.config import get_config
-from pyspark.sql import SparkSession
-from pyspark import SparkConf
-import findspark
 from pathlib import Path
 
-this_dir = Path().cwd().resolve()
+import findspark
+from dotenv import load_dotenv
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+
+from inno_syncer.main import start_job
+from inno_syncer.sync.config import get_config
+
 SPARK_VERSION = "3.3"
 ICEBERG_VERSION = "1.4.3"
+
+this_dir = Path(__file__).parent
+load_dotenv(dotenv_path=Path(".env").resolve())
 
 findspark.init()
 
@@ -17,24 +22,18 @@ conf.set(
     f"org.apache.iceberg:iceberg-spark-runtime-{SPARK_VERSION}_2.12:{ICEBERG_VERSION},com.microsoft.azure:spark-mssql-connector_2.12:1.3.0-BETA",
 )
 conf.set("spark.sql.sources.default", "iceberg")
-# conf.set("spark.jars", f"{this_dir}/jdbc/postgresql-42.7.2.jar")
-# conf.set("spark.driver.extraClassPath", f"{this_dir}/jdbc/postgresql-42.7.2.jar")
-
 conf.set("spark.sql.execution.pyarrow.enabled", "true")
 conf.set("spark.sql.catalog.spark_catalog", "org.apache.iceberg.spark.SparkSessionCatalog")
 conf.set("spark.sql.catalog.spark_catalog.type", "hive")
-conf.set("spark.sql.catalog.spark_catalog.uri", "thrift://localhost:9083")
-# conf.set("spark.sql.catalog.hive_metastore", "org.apache.iceberg.spark.SparkCatalog")
-# conf.set("spark.sql.catalog.hive_metastore.type", "hive")
-# conf.set("spark.sql.catalog.hive_metastore.uri", "thrift://localhost:9083")
+conf.set("hive.metastore.uris", "thrift://localhost:9083")
 conf.set(
     "spark.sql.extensions",
     "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions",
 )
-conf.set("spark.debug.maxToStringFields", "1000")
 spark = SparkSession.builder.config(conf=conf).enableHiveSupport().getOrCreate()
+spark.sparkContext.setLogLevel("ERROR")
 spark.sql("USE spark_catalog;")
 
-production_config = get_config("application-mssql.conf")
+production_config = get_config(f"{this_dir}/application.conf")
 
 start_job(spark, production_config)
--- samples/mysql/driver.py
+++ samples/mysql/driver.py
@@ -1,6 +1,7 @@
-import findspark
 from pathlib import Path
 
+import findspark
+from dotenv import load_dotenv
 from pyspark.sql import SparkSession
 from pyspark import SparkConf
 
@@ -8,6 +9,7 @@ from inno_syncer.main import start_job
 from inno_syncer.sync.config import get_config
 
 this_dir = Path(__file__).parent
+load_dotenv(dotenv_path=Path(".env").resolve())
 
 
 findspark.init()
@@ -17,16 +19,12 @@ conf.set(
     "spark.jars.packages",
     "org.apache.iceberg:iceberg-spark-runtime-3.3_2.12:1.4.3,mysql:mysql-connector-java:8.0.30",
 )
-conf.set("spark.sql.sources.default", "iceberg")
-
 conf.set("spark.sql.execution.pyarrow.enabled", "true")
-conf.set(
-    "spark.sql.catalog.spark_catalog", "org.apache.iceberg.spark.SparkSessionCatalog"
-)
+conf.set("spark.sql.sources.default", "iceberg")
+conf.set("spark.sql.catalog.spark_catalog", "org.apache.iceberg.spark.SparkSessionCatalog")
 conf.set("spark.sql.catalog.spark_catalog.type", "hive")
 conf.set("spark.sql.catalogImplementation", "hive")
 conf.set("hive.metastore.uris", "thrift://localhost:9083")
-
 conf.set(
     "spark.sql.extensions",
     "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions",
@@ -35,8 +33,7 @@ spark = SparkSession.builder.config(conf=conf).enableHiveSupport().getOrCreate()
 spark.sparkContext.setLogLevel("ERROR")
 spark.sql("USE spark_catalog;")
 spark.sql("SHOW DATABASES;").show()
-# spark.sql("DROP DATABASE IF EXISTS mysql_raw CASCADE;")
 
-# production_config = get_config(f"{this_dir}/application.conf")
+production_config = get_config(f"{this_dir}/application.conf")
 
-# start_job(spark, production_config)
+start_job(spark, production_config)
